e4176bd26cddaca4594ed25bdf568a2c732a0656
Seth Broweleit <hello@getsetbro.com>
2014-03-20 13:31:03 -0500

    routing

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,23 +2,23 @@ var app = angular.module('app', ['ui.router']);
 
 app.config(function($stateProvider, $urlRouterProvider) {
 
-	$urlRouterProvider.when('', '/searchform');
+	$stateProvider
+		.state('searchform', {
+			url: '/searchform',
+			templateUrl: 'views/searchform.html',
+			controller: 'searchform'
+		})
+		.state('results', {
+			url: '/results',
+			templateUrl: 'views/results.html',
+			controller: 'results'
+		})
+		.state('call-list', {
+			url: '/call-list',
+			templateUrl: 'views/call-list.html',
+			controller: 'calllist'
+		});
 
-  $stateProvider
-	.state('searchform', {
-		url: '/searchform',
-		templateUrl: 'views/searchform.html',
-		controller: 'searchform'
-	})
-	.state('results', {
-		url: '/results',
-		templateUrl: 'views/results.html',
-		controller: 'results'
-	})
-	.state('call-list', {
-		url: '/call-list',
-		templateUrl: 'views/call-list.html',
-		controller: 'calllist'
-	});
+	$urlRouterProvider.otherwise('/searchform');
 
 });
