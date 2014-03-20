eb1ea6d85c0b6d2fdc8d6784e55fe2d42a9ff80b
Seth Broweleit <hello@getsetbro.com>
2014-03-20 12:12:22 -0500

    ui-sref-active

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,22 +1,24 @@
-var app = angular.module('app', ['ngRoute']);
+var app = angular.module('app', ['ui.router']);
 
-app.config(['$routeProvider',
-	function($routeProvider) {
-		$routeProvider.
-		when('/search', {
-			templateUrl: 'views/searchform.html',
-			controller: 'searchform'
-		}).
-		when('/results', {
-			templateUrl: 'views/results.html',
-			controller: 'results'
-		}).
-		when('/call-list', {
-			templateUrl: 'views/call-list.html',
-			controller: 'calllist'
-		}).
-		otherwise({
-			redirectTo: '/search'
-		});
-	}
-]);
+app.config(function($stateProvider, $urlRouterProvider) {
+
+	$urlRouterProvider.when('', '/searchform');
+
+  $stateProvider
+	.state('searchform', {
+		url: '/searchform',
+		templateUrl: 'views/searchform.html',
+		controller: 'searchform'
+	})
+	.state('results', {
+		url: '/results',
+		templateUrl: 'views/results.html',
+		controller: 'results'
+	})
+	.state('call-list', {
+		url: '/call-list',
+		templateUrl: 'views/call-list.html',
+		controller: 'calllist'
+	});
+
+});
